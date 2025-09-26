e64d2891961564a6f3b5bd012d19e1dad863bc99
Rajat Pandey <rajatpndey257@gmail.com>
2025-09-27 01:51:54 +0530

    ui done

--- React.js Fundamental/Chef_Claude/src/components/Main.jsx
+++ React.js Fundamental/Chef_Claude/src/components/Main.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
+  
   return (
     <div>
-        <form className='flex justify-center items-center mt-10 gap-2'>
-        <input type="text" placeholder='Add  an ingredient...' className='border border-gray-300 rounded-md p-2 flex-1 max-w-md'/>
-        <button type="submit" className='cursor-pointer bg-black text-white px-4 py-2 rounded-md  transition'> + Add ingredient </button>
-        
-        </form>
+      <form className="flex justify-center items-center mt-10 gap-2">
+        <input
+          type="text"
+          placeholder="Add  an ingredient..."
+          className="border border-gray-300 rounded-md p-2 flex-1 max-w-md"
+        />
+        <button
+          type="submit"
+          className="cursor-pointer bg-black text-white px-4 py-2 rounded-md  transition"
+        >
+          {" "}
+          + Add ingredient{" "}
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
