c6195ec823eeadf600cdf3a4656a1f63a3ac5e0f
Rajat Pandey <rajatpndey257@gmail.com>
2025-09-26 22:07:35 +0530

    update form input and button for adding ingredients

--- React.js Fundamental/Chef_Claude/src/components/Main.jsx
+++ React.js Fundamental/Chef_Claude/src/components/Main.jsx
@@ -4,8 +4,8 @@ const Main = () => {
   return (
     <div>
         <form className='flex justify-center items-center mt-10 gap-2'>
-        <input type="text" placeholder='Search recipes...' className='border border-gray-300 rounded-md p-2 flex-1 max-w-md'/>
-        <button type="submit" className='bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition'>Search</button>
+        <input type="text" placeholder='Add  an ingredient...' className='border border-gray-300 rounded-md p-2 flex-1 max-w-md'/>
+        <button type="submit" className='cursor-pointer bg-black text-white px-4 py-2 rounded-md  transition'> + Add ingredient </button>
         
         </form>
     </div>
