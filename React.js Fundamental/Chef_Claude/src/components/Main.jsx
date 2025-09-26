7a4dd04b640f2897164c04bcc845153369fb9073
Rajat Pandey <rajatpndey257@gmail.com>
2025-09-26 16:27:06 +0530

    ui mostly created

--- React.js Fundamental/Chef_Claude/src/components/Main.jsx
+++ React.js Fundamental/Chef_Claude/src/components/Main.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Main = () => {
   return (
     <div>
-        <form>
-        <input type="text" placeholder='Search recipes...' className='border border-gray-300 rounded-md p-2 w-full max-w-md mx-auto mt-10 block'/>
-        <button type="submit" className='mt-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition'>Search</button>
+        <form className='flex justify-center items-center mt-10 gap-2'>
+        <input type="text" placeholder='Search recipes...' className='border border-gray-300 rounded-md p-2 flex-1 max-w-md'/>
+        <button type="submit" className='bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition'>Search</button>
         
         </form>
     </div>
